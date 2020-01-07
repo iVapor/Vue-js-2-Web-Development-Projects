256114f82dc68d100bef7f652ba1c826395cc985
iVapor <yangyy16@outlook.com>
2020-01-07 15:36:30 +0800

    添加动画组

--- Chapter03/chapter3-download/components/ui.js
+++ Chapter03/chapter3-download/components/ui.js
@@ -33,7 +33,9 @@ Vue.component('card', {
 Vue.component('hand', {
     template: `<div class="hand">
         <div class="wrapper">
-            <card v-for="card of cards" :def="card.def" @play="handlePlay(card)" /> 
+            <transition-group name="card" class="cards">
+                <card v-for="card of cards" :def="card.def" @play="handlePlay(card)" :key="card.id"/> 
+            </transition-group>
         </div>
     </div>`,
     props: ['cards'],
